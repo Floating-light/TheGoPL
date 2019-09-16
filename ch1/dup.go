f81717a0dc477dd4b578a8177a8261877df5bab4
Flaoting-light <liuyuanyu12138@gmail.com>
2019-09-16 21:17:14 +0800

    feat fetching a URL

---
+++ ch1/dup.go
@@ -0,0 +1,79 @@
+package ch1
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+//CountInput print the text of each line that appears more than once in the
+//standard input, preceded by its count.
+func CountInput() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	//NOTE: ignore potential errors from input.Err()
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
+//CountInput2 prints the count and text of lines that appear more than once
+//in the input.It reads from stdin or from a list of named files.
+func CountInput2() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, file := range files {
+			f, err := os.Open(file)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "CountInput2 %v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	//NOTE: ignore potential errors from input.Err()
+}
+
+//CountInput3 read the entire input into memory in one big gulp, split it into
+//lines all at once, then process the lines.
+func CountInput3() {
+	counts := make(map[string]int)
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "CountInput3 : %v\n", err)
+			continue
+		}
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/Floating-light/TheGoPL/ch1"
+)
+
+func main() {
+	ch1.FetchExe1_9()
+}
